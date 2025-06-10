9c62819fbe7c5e77d51967b70fdd426b03cdbc35
SandyAntonius <sandyantoni0111@gmail.com>
2025-06-11 00:50:37 +0300

    Create translator.py

---
+++ translator.py
@@ -0,0 +1,10 @@
+#Translator Program 
+from deep_translator import GoogleTranslator
+
+msg = input("Enter a sentence you want to translate: ")
+lang = input('''Enter the language to translate: 
+             (English => en, Arabic => ar, French => fr, German => de, Russian => ru
+              Turkish => tr ): ''')
+
+result = GoogleTranslator(source='auto', target= lang ).translate(msg)
+print(f"Translated message: {result}") 
